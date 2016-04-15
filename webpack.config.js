5aa0cdaf1a967f394c328aae6dad268d886249af
Dave Stevens <dave@dstvns.co.uk>
2016-04-15 11:44:15 +0100

    Enables sass loading.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ var config = {
         include : APP_DIR,
         loader : "babel"
       },
-      { test: /\.css$/, loader: "style-loader!css-loader" }
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" }
     ]
   },
   plugins: [
